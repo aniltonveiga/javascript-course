b2178b3d1af3fc6b7decc813fe7430c6226b61a9
Anilton Veiga <anilton.veigaa@gmail.com>
2020-12-20 01:26:20 -0300

    feat: pratical 01

--- 01-PRATICAL/index.js
+++ 01-PRATICAL/index.js
@@ -13,9 +13,12 @@
 /** 05 - Utilizando a variável da pergunta 04, imprima o nome do segundo familiar armazenado.*/
 
 /** 06 - Crie uma variável chamada "parente" e armazene duas propriedades nome e parentesco (parentesco = pai,
- * mae, tio, tia), imprima na tela o nome e o parentesco do seu parente.*/
+ * mae, tio, tia), imprima na tela o nome, o parentesco e a idade do seu parente.*/
 
 /** 07 - Altere a variável "familia" para receber os 3 familiares, mas agora adicionando para cada familiar além
- * do nome o parentesco, conforme feito na pergunta 05.*/
+ * do nome adicione o parentesco e idade de cada familiar, conforme feito na pergunta 05.*/
 
 /** 08 - Utilizando a variável da pergunta 07, imprima o nome e parentesco do útlimo familiar.*/
+
+/** 09 - Crie uma variável que tenha como objetivo controlar se uma porta esta aberta ou fechada, escolha o tipo
+ * de dado mais adequado.*/
