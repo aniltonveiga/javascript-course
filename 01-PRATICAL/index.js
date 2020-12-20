ad2f48acb22fb096e8760d538f66077c1dce1e5d
Anilton Veiga <anilton.veigaa@gmail.com>
2020-12-20 01:15:19 -0300

    feat: pratical 01

---
+++ 01-PRATICAL/index.js
@@ -0,0 +1,24 @@
+/** Para resolver esses exercícios, adicione a linha ou bloco de código correspondente abaixo do enunciado.  /*
+
+/** 01 - Declare uma variável sem valor que poderá ser acessada de forma "global" por qualquer função dentro do seu arquivo com nome global */
+
+/** 02 - Agora atribua a variavel "global" o valor "Essa é minha variável global" e imprima na tela */
+
+/** 03 - Crie uma variável chamada "saldo" utilizando as boas práticas do JavaScript ES6 e inicialize
+ * ela com o valor de "positivo", logo após altere o seu valor para "negativo". Imprima as duas instruções na tela
+ */
+
+/** 04 - Crie uma variável chamada "familia", e armazene o nome de 3 familiares, e imprima na tela.*/
+
+/** 05 - Utilizando a variável da pergunta 04, imprima o nome do segundo familiar armazenado.*/
+
+/** 06 - Crie uma variável chamada "parente" e armazene duas propriedades nome e parentesco (parentesco = pai,
+ * mae, tio, tia), imprima na tela o nome e o parentesco do seu parente.
+ */
+
+/** 07 - Altere a variável "familia" para receber os 3 familiares, mas agora adicionando para cada familiar além
+ * do nome o parentesco, conforme feito na pergunta 05.
+ */
+
+/** 08 - Utilizando a variável da pergunta 07, imprima o nome e parentesco do útlimo familiar.
+ */
